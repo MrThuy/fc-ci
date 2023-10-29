b92ff68dbc6f4c56b186c3f7729bacf18d9d6abf
Arthur Pickert <mrthuy@hotmail.com>
2023-10-29 20:23:41 -0300

    feat: break test sum

--- js/src/sum.test.js
+++ js/src/sum.test.js
@@ -2,7 +2,7 @@ const sum = require('./sum');
 
 describe('sum function', () => {
   test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3);
+    expect(sum(1, 2)).toBe(4);
   });
 
   test('adds -1 + 1 to equal 0', () => {
